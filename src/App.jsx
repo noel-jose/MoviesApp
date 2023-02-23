ab5cd9b1c35f2ed45647c8bedce55422c1a98741
Noel Jose <noeljose@SSLAP0013.local>
2023-02-23 13:46:05 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.scss';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <h1>This is the heading</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Header"
+import 
+
+function Header() {
+  return (
+    <div className='header'>
+        <img src="" alt="" />
+        <img src="" alt="" />
+    </div>
+  )
+}
+
+export default Header;
